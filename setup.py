66c2c868557d499d015e9464ce573791144ab142
mal-zurich <79984953+mal-zurich@users.noreply.github.com>
2024-06-13 10:59:37 +0200

    Add heterogeneous data-driven workload generator (#19)
    * Workload generator model added
    
    * Workload generator model file added to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name="fmperf",
     version="0.0.1",
-    package_data={"fmperf.data": ["ai.txt"]},
+    package_data={"fmperf.data": ["ai.txt", "all_nbins_64.pkl"]},
     packages=["fmperf", "fmperf.utils", "fmperf.loadgen", "fmperf.data"],
 )
