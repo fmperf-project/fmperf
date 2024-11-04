984f6e0acd8f4de7cb05e1ef19c170a6fabe5245
Ghazi Sarwat Syed <ghs@zurich.ibm.com>
2024-11-04 14:25:27 +0000

    updated generator fix
    Signed-off-by: Ghazi Sarwat Syed <ghs@zurich.ibm.com>

--- fmperf/utils/constants.py
+++ fmperf/utils/constants.py
@@ -2,4 +2,4 @@ import os
 
 REQUESTS_DIR = os.environ.get("REQUESTS_DIR", ".")
 REQUESTS_FILENAME = os.environ["REQUESTS_FILENAME"]
-RESULTS_ALL_FILENAME = os.environ.get("RESULTS_ALL_FILENAME",None)
+RESULTS_ALL_FILENAME = os.environ.get("RESULTS_ALL_FILENAME", None)
